df751399a317cf01fe05c657d172a1d4dccdd29a
mateusadd <mateus.add02@gmail.com>
2022-07-12 20:13:18 -0300

    final

--- decripta.py
+++ decripta.py
@@ -1,31 +1,36 @@
 import text_chunk
 import base64
-import binascii
 
-input = open('./txt_files/codificado.txt').readlines()
-output = open('./txt_files/original.txt', 'w')
+#Entrada dos dados a serem processados
+print('Please enter full path for files!')
+source = input('File to be decrypted: ')
+output = input('File to fill with decrypted version: ')
+keyFile = input('File containing key for encryption: ')
 
-keyFile = open('./txt_files/private.txt')
+#Abrir arquivos informados
+source = open(source).readlines()
+output = open(output, 'w')
+keyFile = open(keyFile)
 
 keys = keyFile.readlines()
 
 mod = int(keys[0])
 key = int(keys[1])
+
+#string a ser preenchida com todo o conteúdo descriptografado
 fullText = ''
 
 #Ler arquivo criptografado
-for line in input:
+for line in source:
+    #calcular
+    originalChunk = pow(int(line), key, mod)
+    #Traduzir BigInt para string
+    chunk = text_chunk._int_to_str(originalChunk)
+    #Formar a string com conteúdo descriptografado
+    fullText += chunk
 
-    #print(line)
-    originalChunk = pow(int(line), mod, key)
-    binascii.unhexlify(hex(originalChunk).strip())
-
+#Transformar base64 no conteúdo original
+plain = base64.b64decode(bytes(fullText.encode())).decode()
     
-
-
-#Traduzir os inteiros
-
-
-#Decodificar
-
-#Escrever no arquivo de saída
+#Escrever versão descriptografada no arquivo de saída
+output.write(str(plain))
--- encripta.py
+++ encripta.py
@@ -1,48 +1,51 @@
-from asyncore import write
 import base64
 import text_chunk
-import binascii
 
-#Ler os arquivos de entrada e chave pública
-input = open('./txt_files/original.txt')
-output = open('./txt_files/codificado.txt', 'w')
-keyFile = open('./txt_files/public.txt')
+#Entrada dos dados a serem processados
+print('Please enter full path for files!')
+source = input('File to be encrypted: ')
+output = input('File to fill with encrypted version: ')
+keyFile = input('File containing key for encryption: ')
+
+#Abrir arquivos informados
+source = open(source)
+output = open(output, 'w')
+keyFile = open(keyFile)
 
 keys = keyFile.readlines()
 
+#Módulo e chave pública
 mod = int(keys[0])
 key = int(keys[1])
 
 #Formar uma string única com o conteúdo do arquivo de entrada
-text = input.readlines()
+text = source.readlines()
 strText = ''
 
 for line in text:
     strText += line
 
-print(strText)
+print('String to be encrypted: '+strText)
 
 #Definir chunkSize a partir do módulo
 chunkSize = text_chunk.block_size(mod)
 print('Defined a ' + str(chunkSize) +' chunkSize')
 
 #Gerar versão codificada da string
-mesBytes = strText.encode()
-b64Bytes = binascii.hexlify(base64.b64encode(mesBytes))
-
-print('Encoded full string is: '+str(b64Bytes))
+encoded = base64.b64encode(bytes(strText.encode())).decode()
 
 #Dividir a versão codificada em chunks com o tamanho definido
-chunks = [b64Bytes[i:i+chunkSize] for i in range(0, len(b64Bytes), chunkSize)]
+chunks = [encoded[i:i+chunkSize] for i in range(0, len(encoded), chunkSize)]
 print(chunks)
 
 #Transformar cada chunk em BigInt e gerar versão codificada
 for chunk in chunks:
-    originalChunk = int(chunk, 32)
-    print(originalChunk)
+    
+    #Gerar BigInt
+    originalChunk = text_chunk.TextChunk(chunk).int_value()
+
+    #Calcular
     encodedChunk = pow(originalChunk, key, mod)
-    print(encodedChunk)
     
     #Escrever versão codificada no arquivo de saída
     output.write(str(encodedChunk)+'\n')
-    
